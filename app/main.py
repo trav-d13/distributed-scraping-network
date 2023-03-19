e375665450ab8bcc230835b527ec4a46fe9189dc
Travis Dawson <t.dawson@student.maastrichtuniversity.nl>
2023-03-19 11:45:15 +0100

    Completed job post specification complete

--- app/main.py
+++ app/main.py
@@ -6,13 +6,60 @@ from typing import List
 class CompletedJob(BaseModel):
     id: int
     lat: float
-    lon: float
-    observed_on: str
+    long: float
+    observed_on: str  # Date and time of observation
+    time_zone: str
     elevation: float
-    time: List[str]
-    temperature_2m: List[float]
-    dewpoint_2m: List[float]
+    time: str  # Time from which daily variables were extracted
 
+    # Daily variables
+    temperature_2m: float
+    relativehumidity_2m: float
+    dewpoint_2m: float
+    apparent_temperature: float
+    surface_pressure: float
+    precipitation: float
+    rain: float
+    snowfall: float
+    cloudcover: float
+    cloudcover_low: float
+    cloudcover_mid: float
+    cloudcover_high: float
+    shortwave_radiation: float
+    direct_radiation: float
+    diffuse_radiation: float
+    windspeed_10m: float
+    windspeed_100m: float
+    winddirection_10m: float
+    winddirection_100m: float
+    windgusts_10m: float
+    et0_fao_evapotranspiration_hourly: float
+    weathercode_hourly: int
+    vapor_pressure_deficit: float
+    soil_temperature_0_to_7cm: float
+    soil_temperature_7_to_28cm: float
+    soil_temperature_28_to_100cm: float
+    soil_moisture_0_to_7cm: float
+    soil_moisture_7_to_28cm: float
+    soil_moisture_28_to_100cm: float
+
+    # Daily variables
+    weathercode_daily: int
+    temperature_2m_max: float
+    temperature_2m_min: float
+    apparent_temperature_max: float
+    apparent_temperature_min: float
+    precipitation_sum: float
+    rain_sum: float
+    snowfall_sum: float
+    precipitation_hours: float
+    sunrise: str
+    sunset: str
+    windspeed_10m_max: float
+    windgusts_10m_max: float
+    winddirection_10m_dominant: float
+    shortwave_radiation_sum: float
+    et0_fao_evapotranspiration_daily: float
 
 class JobInfo(BaseModel):
     id: int
